912ab182ecffaf70a196e8e8448d38df31680ad1
Vladimir Jovičić <vladimir.jovicic@j-rabbit.net>
2024-04-19 14:06:01 +0200

    cache: add cache global cache service for handling big data much faster
    wip

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { Vehicle } from '../model/vehicle/vehicle';
+
+/**
+ * Service that contains the cache and methods for manipulating the cache
+ */
+@Injectable()
+export class CacheService {
+  private cache: Map<string, Vehicle> = new Map();
+
+  /**
+   * Get the whole cashe data
+   * @returns The whole cache
+   */
+  public getAll(): Vehicle[] {
+    return Array.from(this.cache.values());
+  }
+
+  /**
+   * Sets an individual vehicle
+   * @param id The id of the vehicle
+   * @param data The vehicle data to set
+   */
+  public set(id: string, data: Vehicle): void {
+    this.cache.set(id, data);
+  }
+
+    /**
+   * Gets an individual vehicle
+   * @param id The id of the vehicle
+   */
+    public get(id: string): Vehicle | null {
+      return this.cache.get(id) || null;
+    }
+
+  /**
+   * Checks if the cache data is empty
+   * @returns true if the cache is empty. Otherwise false
+   */
+  public isEmpty(): boolean {
+    return this.cache.size === 0;
+  }
+
+  /**
+   * Adds the whole array to the cache
+   * @param vehicles The array to add
+   */
+  public addAll(vehicles: Vehicle[]): void {
+    vehicles.forEach(v => this.set(v.id, v));
+  }
+}
